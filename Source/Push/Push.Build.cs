2aea8c1cfada09241323da206f1367a2d11bb253
andrescrd <c.andresricardo@gmail.com>
2020-09-02 00:34:28 -0300

    fux unreal Ed dependency

--- Source/Push/Push.Build.cs
+++ Source/Push/Push.Build.cs
@@ -8,7 +8,7 @@ public class Push : ModuleRules
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UnrealEd", "UMG" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
 
         PrivateDependencyModuleNames.AddRange(new string[] { });
 
