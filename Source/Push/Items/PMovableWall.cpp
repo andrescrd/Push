000c42efe87089c709b6446c655d06e66068cd98
andrescrd <c.andresricardo@gmail.com>
2020-10-03 00:24:56 -0300

    adding movable wall

---
+++ Source/Push/Items/PMovableWall.cpp
@@ -0,0 +1,91 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PMovableWall.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/TimelineComponent.h"
+#include "Components/SceneComponent.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/Character.h"
+#include "Engine/Engine.h"
+
+// Sets default values
+APMovableWall::APMovableWall()
+{
+	ZOffset = -250.f;
+	USceneComponent *Dummy = CreateDefaultSubobject<USceneComponent>(TEXT("Dummy"));
+	RootComponent = Dummy;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetupAttachment(RootComponent);
+
+	ActivatorComp = CreateDefaultSubobject<UBoxComponent>(TEXT("ActivatorComp"));
+	ActivatorComp->SetupAttachment(RootComponent);
+
+	MyTimeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("Timelines"));
+}
+
+// Called when the game starts or when spawned
+void APMovableWall::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ActivatorComp->OnComponentBeginOverlap.AddDynamic(this, &APMovableWall::HandleBeginOverlap);
+	ActivatorComp->OnComponentEndOverlap.AddDynamic(this, &APMovableWall::HandleEndOverlap);
+	SetupTimeline();
+}
+
+void APMovableWall::SetupTimeline()
+{
+	if (Curve)
+	{
+		FOnTimelineFloat InterpFunction;
+		FOnTimelineEvent TimelineFinished;
+
+		InterpFunction.BindUFunction(this, FName("OnTimelineFloatReturn"));
+		TimelineFinished.BindUFunction(this, FName("OnTimelineFinished"));
+
+		MyTimeline->AddInterpFloat(Curve, InterpFunction, FName("Alpha"));
+		MyTimeline->SetTimelineFinishedFunc(TimelineFinished);
+
+		StartLocation =MeshComp->GetRelativeLocation();
+		EndLocation = StartLocation;
+		EndLocation.Z += ZOffset;
+
+		MyTimeline->SetLooping(false);
+		MyTimeline->SetIgnoreTimeDilation(true);
+	}
+}
+
+void APMovableWall::HandleBeginOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
+{
+	if (ACharacter* Character = Cast<ACharacter>(OtherActor))
+		StartMovement();
+}
+
+void APMovableWall::HandleEndOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex)
+{
+	if (ACharacter* Character = Cast<ACharacter>(OtherActor))
+		ReverseMovement();
+}
+
+void APMovableWall::OnTimelineFloatReturn(float Value)
+{
+	MeshComp->SetRelativeLocation(FMath::Lerp(StartLocation, EndLocation, Value));
+}
+
+void APMovableWall::OnTimelineFinished()
+{
+	//TODO: Do something here
+}
+
+void APMovableWall::StartMovement()
+{
+	if (Curve)
+		MyTimeline->Play();
+}
+
+void APMovableWall::ReverseMovement()
+{
+	if (Curve)
+		MyTimeline->Reverse();
+}
---
+++ Source/Push/Items/PMovableWall.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PMovableWall.generated.h"
+
+UCLASS()
+class PUSH_API APMovableWall : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APMovableWall();
+
+protected:
+	FVector StartLocation;
+	FVector EndLocation;
+
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	class UStaticMeshComponent *MeshComp;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	class UTimelineComponent *MyTimeline;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)	
+	class UBoxComponent * ActivatorComp;
+
+	void SetupTimeline();
+
+public:
+	UPROPERTY(EditAnywhere, Category = "Timeline")
+	class UCurveFloat *Curve;
+	UPROPERTY(EditAnywhere, Category = "Timeline")
+	float ZOffset;
+
+	UFUNCTION()
+	void HandleBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	UFUNCTION()
+	void HandleEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	UFUNCTION()
+	void OnTimelineFloatReturn(float Value);
+	UFUNCTION()
+	void OnTimelineFinished();
+
+	UFUNCTION(BlueprintCallable)
+	void StartMovement();
+
+	UFUNCTION(BlueprintCallable)
+	void ReverseMovement();
+};
